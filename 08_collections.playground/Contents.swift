0262cd0382c092406e9c1d02e8c92c6fcf7d9bf0
JosefDeveloper <jfelix0107@gmail.com>
2022-06-12 15:09:48 -0500

    Arrays

--- 08_collections.playground/Contents.swift
+++ 08_collections.playground/Contents.swift
@@ -1,5 +1,29 @@
 import UIKit
 
 // Arrays
+var someInts = [Int]()
+someInts = []
+someInts.append(31)
+someInts.count
+
+var someDoubles = Array(repeating: 3.141592, count: 3)
+someDoubles.count
+
+var moreDoubles = Array(repeating: 2.5, count: 4)
+
+var aLotOfDoubles = someDoubles + moreDoubles
+aLotOfDoubles.count
+
+var shoppingList : [String] = ["Papas", "Pimiento", "Tortillas", "Cerdo", "Cebolla"]
+shoppingList.count
+
+// Reto
+var myFamily = ["Papá", "Mamá", "Ale", "Simba", "Nala"]
+myFamily.count
+
+for eachMemberFamily in myFamily {
+    print("\(eachMemberFamily) 💚")
+}
+
 // Conjuntos
 // Diccionarios
